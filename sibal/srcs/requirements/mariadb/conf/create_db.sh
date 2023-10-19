64341228f258b8fdf541395602e2e3b3171f070f
Nayun Ki <naki@c3r1s3.42seoul.kr>
2023-10-19 23:04:27 +0900

    fin

--- sibal/srcs/requirements/mariadb/conf/create_db.sh
+++ sibal/srcs/requirements/mariadb/conf/create_db.sh
@@ -1,8 +1,7 @@
-#!bin/sh
+#mkdir -p /auth_pam_tool_dir
+#touch /auth_pam_tool_dir/auth_pam_tool
 
-#make user and
-#set all permission
-#set root passwd
+# /tmp/init_db.sql 파일 생성, 아래 내용을 해당 파일에 적는다.
 cat > /tmp/create_db_sql << EOF
 USE mysql;
 FLUSH PRIVILEGES;
@@ -15,4 +14,6 @@ EOF
 
 # run init.sql
 /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db_sql
+# mysql DB서버 초기화를 위해 --bootstrap 옵션과 함께 실행. 위 스크립트를 사용하여 DB 설정
 /usr/bin/mysqld -u mysql
+# 초기화된 MySQL DB 서버를 실행. mysql 사용자로 실행한다.
